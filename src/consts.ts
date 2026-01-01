4d23057579c76fc72811f54da4f712082d170ebe
roomy-tadpole2 <1402829786@qq.com>
2026-01-01 17:49:57 +0800

    26-1-16 12:34:56 +0000 Initial commit of new blog layout and components

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+// export const SITE_TITLE = 'Le brouillon d\'Adam';
+export const SITE_TITLE = '';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const Scroll_Trigger_Const = 100;
