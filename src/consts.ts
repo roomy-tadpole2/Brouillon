a0dd03848ad0e6b74e1796888ff93b39716d395c
roomy-tadpole2 <1402829786@qq.com>
2026-01-09 17:56:20 +0800

    1-9

--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 // export const SITE_TITLE = 'Le brouillon d\'Adam';
-export const SITE_TITLE = '';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-export const Scroll_Trigger_Const = 100;
+export const SITE_TITLE = 'Adam\'s draft';
+export const SITE_DESCRIPTION = 'drafts';
+export const Scroll_Trigger_Const = 50;
+export const Motion_Trigger_Const = 1200;
